c2d6f2f03c7ccd894f421700ac929f39d20d2899
FANX1NG <2395952316@qq.com>
2019-05-04 08:51:56 +0800

    优化城市管理界面以及删除逻辑

--- app/src/main/java/com/fanxing/fxweather2/CityManagerActivity.java
+++ app/src/main/java/com/fanxing/fxweather2/CityManagerActivity.java
@@ -5,15 +5,18 @@ import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.fanxing.fxweather2.SQLite.MySQLiteOpenHelper;
 import com.fanxing.fxweather2.adapter.CityManagerAdapter;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,6 +30,10 @@ public class CityManagerActivity extends AppCompatActivity {
     ImageButton ibToolbarOut;
     @BindView(R.id.lv_city_manager)
     ListView lvCityManager;
+    @BindView(R.id.tv_edit)
+    TextView tvEdit;
+    @BindView(R.id.tv_del)
+    TextView tvDel;
     private List<Map<String, String>> paths = new ArrayList<>();
     private SQLiteDatabase db;
     private HashMap<String, String> map;
@@ -47,34 +54,30 @@ public class CityManagerActivity extends AppCompatActivity {
         //删除数据监听
         lvCityManager.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView,final View view,final int i, long l) {
-                new AlertDialog.Builder(CityManagerActivity.this).setTitle("城市删除提示").setMessage("确认删除"+((TextView)view.findViewById(R.id.tv_cityt)).getText().toString()+"?")
+            public boolean onItemLongClick(AdapterView<?> adapterView, final View view, final int i, long l) {
+                //如果编辑按钮状态为1，则可长按删除
+                if (adapter.editStatus == 1) {
+                    new AlertDialog.Builder(CityManagerActivity.this).setTitle("城市删除提示").setMessage("确认删除" + ((TextView) view.findViewById(R.id.tv_cityt)).getText().toString() + "?")
 
-                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                db.delete("city","cid=?", new String[]{((TextView)view.findViewById(R.id.tv_city_cid)).getText().toString()});
-                                paths.remove(i);
-                                adapter.updateList(paths);
+                            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    db.delete("city", "cid=?", new String[]{((TextView) view.findViewById(R.id.tv_city_cid)).getText().toString()});
+                                    paths.remove(i);
+                                    adapter.updateList(paths, false);
 //                Toast.makeText(CityManagerActivity.this, ""+i+"     "+view.toString(), Toast.LENGTH_SHORT).show();
 
 
-                            }})
-                        .setNegativeButton("取消",null)
-                        .show();
+                                }
+                            })
+                            .setNegativeButton("取消", null)
+                            .show();
+                }
                 return false;
             }
         });
-//        lvCityManager.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                db.delete("city","cid=?", new String[]{((TextView)view.findViewById(R.id.tv_city_cid)).getText().toString()});
-//                paths.remove(i);
-//                adapter.updateList(paths);
-////                Toast.makeText(CityManagerActivity.this, ""+i+"     "+view.toString(), Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-        delData();
+
+        backActivity();
+        editAndDel();
     }
 
     @Override
@@ -82,6 +85,7 @@ public class CityManagerActivity extends AppCompatActivity {
         super.onDestroy();
         db.close();
     }
+
     //回退键监听
     @Override
     public void onBackPressed() {
@@ -90,9 +94,9 @@ public class CityManagerActivity extends AppCompatActivity {
         super.onBackPressed();
 
     }
-    private void delData() {
 
-
+    //返回按钮监听
+    private void backActivity() {
         ibToolbarOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -101,6 +105,92 @@ public class CityManagerActivity extends AppCompatActivity {
             }
         });
     }
+
+    //编辑删除按钮监听
+    private void editAndDel() {
+        tvEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (adapter.editStatus == 1) {
+                    adapter.editStatus = 2;
+                    tvEdit.setText("取消");
+                    ibToolbarOut.setVisibility(View.GONE);
+                    tvDel.setVisibility(View.VISIBLE);
+                    adapter.updateList(paths, true);
+
+                } else if (adapter.editStatus == 2) {
+                    adapter.editStatus = 1;
+                    tvEdit.setText("编辑");
+                    for (int i = 0; i < adapter.checkboxList.size(); i++) {
+                        adapter.checkboxList.get(i).setChecked(false);
+                    }
+                    for (int i = 0; i < adapter.checkboxList.size(); i++) {
+                        adapter.checkboxList.get(i).setChecked(false);
+                    }
+                    for (int i = 0; i < adapter.checkboxList.size(); i++) {
+                        adapter.checkboxList.get(i).setChecked(false);
+                    }
+                    for (int i = 0; i < adapter.checkboxList.size(); i++) {
+                        adapter.checkboxList.get(i).setChecked(false);
+                    }
+                    ibToolbarOut.setVisibility(View.VISIBLE);
+                    tvDel.setVisibility(View.GONE);
+                    adapter.selectId.clear();
+                    adapter.mid.clear();
+                    adapter.checkboxList.clear();
+                    adapter.updateList(paths, false);
+
+                }
+            }
+        });
+        tvDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (adapter.selectId.size() == 0) {
+                    Toast.makeText(CityManagerActivity.this, "选择数为0", Toast.LENGTH_SHORT).show();
+                } else {
+                    new AlertDialog.Builder(CityManagerActivity.this).setTitle("城市删除提示").setMessage("确认删除?")
+
+                            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    ibToolbarOut.setVisibility(View.VISIBLE);
+                                    tvDel.setVisibility(View.GONE);
+                                    adapter.editStatus = 1;
+                                    tvEdit.setText("编辑");
+                                    for (int i = 0; i < adapter.selectId.size(); i++) {
+                                        db.delete("city", "cid=?", new String[]{adapter.selectId.get(i)});
+                                    }
+                                    for (int i = 0; i < adapter.checkboxList.size(); i++) {
+                                        adapter.checkboxList.get(i).setChecked(false);
+                                    }
+                                    for (int i = 0; i < adapter.checkboxList.size(); i++) {
+                                        adapter.checkboxList.get(i).setChecked(false);
+                                    }
+                                    for (int i = 0; i < adapter.checkboxList.size(); i++) {
+                                        adapter.checkboxList.get(i).setChecked(false);
+                                    }
+                                    for (int i = 0; i < adapter.checkboxList.size(); i++) {
+                                        adapter.checkboxList.get(i).setChecked(false);
+                                    }
+                                    paths.clear();
+                                    intiData();
+                                    adapter.selectId.clear();
+                                    adapter.mid.clear();
+                                    adapter.checkboxList.clear();
+                                    adapter.updateList(paths, false);
+
+                                }
+                            })
+                            .setNegativeButton("取消", null)
+                            .show();
+
+                }
+
+            }
+        });
+
+    }
+
 
     private void intiData() {
         // 通过游标遍历1个名为city的表
@@ -119,7 +209,7 @@ public class CityManagerActivity extends AppCompatActivity {
             result.moveToNext();
         }
         result.close();
-        adapter.updateList(paths);
+        adapter.updateList(paths, false);
 
     }
 
--- app/src/main/java/com/fanxing/fxweather2/adapter/CityManagerAdapter.java
+++ app/src/main/java/com/fanxing/fxweather2/adapter/CityManagerAdapter.java
@@ -6,7 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fanxing.fxweather2.CityManagerActivity;
 import com.fanxing.fxweather2.R;
@@ -22,15 +25,23 @@ import java.util.Map;
 public class CityManagerAdapter extends BaseAdapter {
     private List<Map<String, String>> mData = new ArrayList<>();
     private Context context;
+    private boolean flage;
+    public int editStatus = 1;
+    //已被选择城市的id
+    public List<String> selectId = new ArrayList<>();
+    //已被选择的按钮
+    public List<CheckBox> checkboxList = new ArrayList<>();
+    public List<Integer> mid = new ArrayList<>();
 
     public CityManagerAdapter(Context context) {
         this.context = context;
     }
 
-    public void updateList(List<Map<String, String>> arrayList) {
+    public void updateList(List<Map<String, String>> arrayList, boolean flage) {
         if (arrayList == null) return;
         this.mData.clear();
         this.mData.addAll(arrayList);
+        this.flage = flage;
         notifyDataSetChanged();
     }
 
@@ -50,17 +61,55 @@ public class CityManagerAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        ViewHolder holder;
+    public View getView(final int i, View view, ViewGroup viewGroup) {
+        final ViewHolder holder;
         if (view == null) {
             view = LayoutInflater.from(context).inflate(R.layout.citymanager_item, viewGroup, false);
             holder = new ViewHolder();
             holder.mCityCid = (TextView) view.findViewById(R.id.tv_city_cid);
             holder.mCityT = (TextView) view.findViewById(R.id.tv_cityt);
+            holder.mCBselect = view.findViewById(R.id.checkbox_select);
             view.setTag(holder);
         } else {
             holder = (ViewHolder) view.getTag();
         }
+        // 根据flage来设置checkbox的显示状况
+        if (flage) {
+            holder.mCBselect.setVisibility(View.VISIBLE);
+        } else {
+            holder.mCBselect.setVisibility(View.INVISIBLE);
+        }
+        checkboxList.add(holder.mCBselect);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editStatus == 2) {
+                    if (holder.mCBselect.isChecked()) {
+                        holder.mCBselect.setChecked(false);
+                    } else {
+                        holder.mCBselect.setChecked(true);
+                    }
+                }
+            }
+        });
+        //给选择按钮设置点击事件
+        holder.mCBselect.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    selectId.add(holder.mCityCid.getText().toString());
+                    mid.add(i);
+                } else {
+                    selectId.remove(mid.indexOf(i));
+                    mid.remove(mid.indexOf(i));
+
+                }
+            }
+
+
+        });
+
         holder.mCityT.setText(mData.get(i).get("city"));
         holder.mCityCid.setText(mData.get(i).get("cid"));
 
@@ -70,5 +119,6 @@ public class CityManagerAdapter extends BaseAdapter {
     class ViewHolder {
         TextView mCityT;
         TextView mCityCid;
+        CheckBox mCBselect;
     }
 }
